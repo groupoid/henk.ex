5c9343ad60282a23d50a7d18da2862465a3ffdba
Namdak Tonpa <maxim@synrc.com>
2015-12-19 08:02:44 +0200

    code

--- src/om.erl
+++ src/om.erl
@@ -8,17 +8,15 @@ main(A)    -> mad:main(A).
 start()    -> start(normal,[]).
 start(_,_) -> supervisor:start_link({local,om},om,[]).
 stop(_)    -> ok.
-init([]) -> {ok, {{one_for_one, 5, 10}, []}}.
-
-read() -> Bin = file(),
-          io:format("~ts~n",[unicode:characters_to_list(Bin)]), om_tok:tokens(Bin,0,{1,[]},[]).
-          main() -> io:fwrite("~120p~n",[om_parse:expr(read(),[])]).
-
-
-file() ->  {ok,Bin2} = file:read_file(%code:priv_dir(om)++
-                                      "priv/fun.txt"),
-          %<<"( ( a → (b x) → x) → ( c → d ) ) ( a b ) a b"/utf8>>.
-          %<<"(c → (a → c) → v)"/utf8>>.
-%          <<"(λ (a: c → (a (x a)) → v) → λ (a:*) → *)"/utf8>>.
-          Bin2.
+init([])   -> {ok, {{one_for_one, 5, 10}, []}}.
+main()     -> Term=om_parse:expr(read(),[]),
+              io:fwrite("~120p~n~p~n",[Term,file:write_file("priv/fun.om",term_to_binary(Term))]).
+read()     -> Bin = file(),
+              io:format("~ts~n",[unicode:characters_to_list(Bin)]),
+              om_tok:tokens(Bin,0,{1,[]},[]).
+file()     -> {ok,Bin2} = file:read_file("priv/fun.txt"),
+              %<<"( ( a → (b x) → x) → ( c → d ) ) ( a b ) a b"/utf8>>.
+              %<<"(c → (a → c) → v)"/utf8>>.
+              %<<"(λ (a: c → (a (x a)) → v) → λ (a:*) → *)"/utf8>>.
+              Bin2.
 
--- src/om_parse.erl
+++ src/om_parse.erl
@@ -16,14 +16,14 @@ expr([star    |T], Acc) -> expr(T,[{const,star}|Acc]);
 expr([open    |T], Acc) -> expr(T,[{open}|Acc]);
 expr([arrow   |T], Acc) -> expr(T,[{arrow}|Acc]);
 expr([lambda  |T], Acc) -> expr(T,[{lambda}|Acc]);
-expr([pi  |T], Acc)     -> expr(T,[{lambda}|Acc]);
+expr([pi  |T], Acc)     -> expr(T,[{pi}|Acc]);
 expr([{name,L},colon|T],Acc) -> expr(T,[{typevar,L}|Acc]);
 expr([{name,L}|T],      Acc) -> expr(T,[{var,L}|Acc]).
 
 rewind([{arrow},{C,Y}|Acc],T, [{N,X}|Rest]) -> rewind(Acc,T,[{arrow,{{C,Y},{N,X}}}|Rest]);
-rewind([{lambda}     |Acc],T, [{arrow,{{app,{{typevar,Label},X}},Y}}|Rest]) -> rewind(Acc,T,[{lambda,{{arg,Label},X,Y}}|Rest]);
-rewind([{N,X}        |Acc],T, [{C,Y}|Rest]) -> rewind(Acc,T,[{app,{{N,X},{C,Y}}}|Rest]);
-rewind([{N,X}        |Acc],T, Rest) -> rewind(Acc,T,[{N,X}|Rest]);
-rewind([{open}       |Acc],T, Rest) -> {T,lists:flatten([Rest|Acc])}.
+rewind([{Fun}|Acc],T, [{arrow,{{app,{{typevar,Label},X}},Y}}|Rest]) when Fun==lambda;Fun==pi -> rewind(Acc,T,[{Fun,{{arg,Label},X,Y}}|Rest]);
+rewind([{N,X}|Acc],T, [{C,Y}|Rest]) -> rewind(Acc,T,[{app,{{N,X},{C,Y}}}|Rest]);
+rewind([{N,X}|Acc],T, Rest) -> rewind(Acc,T,[{N,X}|Rest]);
+rewind([{open}|Acc],T, Rest) -> {T,lists:flatten([Rest|Acc])}.
 
 
