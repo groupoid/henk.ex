d38090c0afc08b7ee3186e89dcefc40988660489
Namdak Tonpa <maxim@synrc.com>
2015-12-21 19:34:23 +0200

    indexed variables in lexer

--- src/om_tok.erl
+++ src/om_tok.erl
@@ -1,14 +1,20 @@
 -module(om_tok).
 -compile(export_all).
 -define(is_space(C), C =:= $\r; C =:= $\s; C =:= $\t).
--define(is_alpha(C), C >= $a, C =< $z; C >= $A, C =< $Z; C >= $0, C =< $9).
+-define(is_alpha(C), C >= $a, C =< $z; C >= $A, C =< $Z; C >= $0, C =< $9; C=:=$@).
 -define(is_termi(C), C =:= $!; C =:= $#; C =:= $$; C =:= $%; C =:= $&; C =:= $(; C =:= $:;
                      C =:= $+; C =:= $-; C =:= $*; C =:= $/; C =:= $.; C =:= $\\;C =:= $);
-                     C =:= $<; C =:= $>; C =:= $=; C =:= $|; C =:= $^; C =:= $~; C =:= $@).
+                     C =:= $<; C =:= $>; C =:= $=; C =:= $|; C =:= $^; C =:= $~).
+
+% om parses depends on only three functions:
+
+rev(X)      -> lists:reverse(X).
+flat(X)     -> lists:flatten(X).
+tokens(X,Y) -> string:tokens(X,Y).
 
 tokens(<<>>,                  _, {_,C}, Acc) -> lists:reverse(stack(C,Acc));
 tokens(<<$\n,     R/binary>>, L, {_,C}, Acc) -> tokens(R,L+1,{1,[]},   stack(C,Acc));
-tokens(<<$(,      R/binary>>, L, {t,C}, Acc) -> tokens(R,L,{t,[$(]},   Acc);
+tokens(<<$(,      R/binary>>, L, {t,_}, Acc) -> tokens(R,L,{t,[$(]},   Acc);
 tokens(<<$),      R/binary>>, L, {t,C}, Acc) -> tokens(R,L,{t,[$)|C]}, Acc);
 tokens(<<$(,      R/binary>>, L, {_,C}, Acc) -> tokens(R,L,{t,[]},     [open   | stack(C,  Acc)]);
 tokens(<<$),      R/binary>>, L, {_,C}, Acc) -> tokens(R,L,{t,[]},     [close  | stack(C,  Acc)]);
@@ -23,12 +29,16 @@ tokens(<<X,       R/binary>>, L, {t,C}, Acc) when ?is_termi(X) -> tokens(R,L,{t,
 tokens(<<X,       R/binary>>, L, {_,C}, Acc) when ?is_termi(X) -> tokens(R,L,{t,[X]},    stack(C, [Acc]));
 tokens(<<X,       R/binary>>, L, {_,C}, Acc) when ?is_space(X) -> tokens(R,L,{s,[C]},              Acc).
 
-stack(C,Acc) ->
-   case lists:flatten(C) of
-        [] -> Acc;
-        "(" -> lists:flatten([open|Acc]);
-        ")" -> lists:flatten([close|Acc]);
-        [X|Aa] when ?is_alpha(X) -> lists:flatten([{name,lists:reverse([X|Aa])}|Acc]);
-        [X|Aa] when ?is_termi(X) -> lists:flatten([{name,lists:reverse([X|Aa])}|Acc]);
-        Re -> lists:flatten([list_to_atom(lists:reverse(Re))|Acc]) end.
+stack(C,Acc) -> case lists:flatten(C) of  []  -> Acc;
+                                          "(" -> [open|Acc];
+                                          ")" -> [close|Acc];
+                     [X|Aa] when ?is_alpha(X) -> var([X|Aa],Acc);
+                     [X|Aa] when ?is_termi(X) -> name([X|Aa],Acc);
+                                           X  -> atom(X,Acc) end.
 
+atom(X,Acc) -> flat([list_to_atom(rev(X))|Acc]).
+name(X,Acc) -> flat([{name,rev(X)}|Acc]).
+ivar([N,I]) -> [N,I];
+ivar([N])   -> [N,"0"].
+var(X,Acc)  -> [Name,Index]= ivar(tokens(rev(X),"@")),
+               flat([{name,{list_to_atom(Name),list_to_integer(Index)}}|Acc]).
