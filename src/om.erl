4f7455c76157e2741f5776dd19cd90663f81f870
Namdak Tonpa <maxim@synrc.com>
2015-12-22 09:45:47 +0200

    readme

--- src/om.erl
+++ src/om.erl
@@ -5,28 +5,6 @@
 -export([init/1, start/2, stop/1]).
 -compile(export_all).
 
-% > om:a("#List/map") == om:type("List/map").
-% true
-% > om:a("\\(x:*)->\\(y:#List/map)->y").
-% {lambda,{{arg,x},
-%          {const,star},
-%          {lambda,{{arg,y},
-%                   {lambda,{{arg,a},
-%                            {const,star},
-%                            {lambda,{{arg,b},
-%                                     {const,star},
-%                                     {lambda,{{arg,f},...
-% > om:type("List/@").
-% {lambda,{{arg,a},
-%         {const,star},
-%         {pi,{{arg,'List'},
-%              {const,star},
-%              {pi,{{arg,'Cons'},
-%                   {pi,{{arg,head},
-%                        {var,{a,0}},
-%                        {pi,{{arg,tail},{var,{'List',0}},{var,{'List',0}}}}}},
-%                   {pi,{{arg,'Nil'},{var,{'List',0}},{var,{'List',0}}}}}}}}}}
-
 a(X)        -> {_,[R]}=om_parse:expr(flat([om:str(X)]),[]),R.
 main(A)     -> mad:main(A).
 start()     -> start(normal,[]).
