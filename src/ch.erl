bb380e904a53265e15f04a2aa5c4494fcb17dc5f
Namdak Tonpa <maxim@synrc.com>
2016-01-12 18:19:30 +0200

    preserve variable naming as ctors

--- src/ch.erl
+++ src/ch.erl
@@ -7,12 +7,16 @@ ap(Fun,Args) -> lists:foldl(fun(X,Acc) -> Acc(X) end,Fun,Args).
 id   () -> fun (X) -> X end.
 inc  () -> fun (X) -> X + 1 end.
 
-nat  () -> [{X,?MODULE:X()}||X<-[zero,succ]].
-zero () ->            fun (F) -> fun (X) -> X end end.
-succ () -> fun (N) -> fun (F) -> fun (X) -> ap(F,[ap(N,[F,X])]) end end end.
+nat  () -> [{X,?MODULE:X()}||X<-[succ,zero]].
+zero () ->              fun (Succ) -> fun (Zero) -> Zero end end.
+succ () -> fun (Nat) -> fun (Succ) -> fun (Zero) -> ap(Succ,[ap(Nat,[Succ,Zero])]) end end end.
+
+list () -> [{X,?MODULE:X()}||X<-[cons,nil]].
+nil  () ->                          fun (Cons) -> fun (Nil) -> Nil end end.
+cons () -> fun (A) -> fun (List) -> fun (Cons) -> fun (Nil) -> [A|[]] end end end end.
 
 church (0) -> zero();
-church (I) -> fun (F) -> fun(X) -> ap(F,[ap(church(I-1),[F,X])]) end end.
+church (I) -> fun (Succ) -> fun(Zero) -> ap(F,[ap(church(I-1),[Succ,Zero])]) end end.
 
 unchurch (N) -> ap(N,[inc(),0]).
 
