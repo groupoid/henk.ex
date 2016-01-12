ba064dfa41599e55edef06f11e17607c5ae59152
Namdak Tonpa <maxim@synrc.com>
2016-01-12 13:57:40 +0200

    ch

--- src/ch.erl
+++ src/ch.erl
@@ -1,23 +1,21 @@
 -module(ch).
--compile({parse_transform, ch}).
--export([parse_transform/2]).
+%-compile([{parse_transform, pi}]).
 -compile(export_all).
-
-parse_transform(Forms, _Options) ->
-    io:format("Forms: ~p~n",[Forms]),
-    compile:forms(Forms,[binary,export_all]),
-    Forms.
 
 ap(Fun,Args) -> lists:foldl(fun(X,Acc) -> Acc(X) end,Fun,Args).
 
 id   () -> fun (X) -> X end.
 inc  () -> fun (X) -> X + 1 end.
-zero ()       -> fun (F) -> fun(X) -> X end end.
-successor (N) -> fun (F) -> fun(X) -> ap(F,[ap(N,[F,X])]) end end.
-unchurch  (N) ->                      ap(N,[inc(),0]).
-church (0)    -> zero();
-church (I)    -> fun (F) -> fun(X) -> ap(F,[ap(church(I-1),[F,X])]) end end.
+
+nat  () -> [X()||X<-[zero,succ]].
+zero () -> fun (F) -> fun (X) -> X end end.
+succ () -> fun (N) -> fun (F) -> fun(X) -> ap(F,[ap(N,[F,X])]) end end end.
+
+church (0) -> zero();
+church (I) -> fun (F) -> fun(X) -> ap(F,[ap(church(I-1),[F,X])]) end end.
+
+unchurch (N) -> ap(N,[inc(),0]).
 
 main()        -> io:format("Zero: ~p~n", [unchurch(zero())]),
-                 io:format("20: ~p~n",   [unchurch(church(20))]),
-                 io:format("Two: ~p~n",  [unchurch(successor(successor(zero())))]).
+                 io:format("20: ~p~n",   [unchurch(church(100000))]),
+                 io:format("Two: ~p~n",  [unchurch(ap(succ(),[ap(succ(),[zero()])]))]).
