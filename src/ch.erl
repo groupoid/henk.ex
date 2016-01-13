7cc8e83266dc56db301aa9c1533bdd2bc2a67063
Namdak Tonpa <maxim@synrc.com>
2016-01-13 15:22:33 +0200

    tikz

--- src/ch.erl
+++ src/ch.erl
@@ -7,19 +7,19 @@ ap(Fun,Args) -> lists:foldl(fun(X,Acc) -> Acc(X) end,Fun,Args).
 id   () -> fun (X) -> X end.
 inc  () -> fun (X) -> X + 1 end.
 
-nat  () -> [{X,?MODULE:X()}||X<-[succ,zero]].
+nat_ () -> [{X,?MODULE:X()}||X<-[nat,succ,zero]].
+nat  () -> fun (Nat) -> fun (Succ) -> fun (Zero) -> Nat end end end.
 zero () ->              fun (Succ) -> fun (Zero) -> Zero end end.
 succ () -> fun (Nat) -> fun (Succ) -> fun (Zero) -> ap(Succ,[ap(Nat,[Succ,Zero])]) end end end.
 
-list () -> [{X,?MODULE:X()}||X<-[cons,nil]].
+list_() -> [{X,?MODULE:X()}||X<-[list,cons,nil]].
+list () ->            fun (List) -> fun (Cons) -> fun (Nil) -> List end end end.
 nil  () ->                          fun (Cons) -> fun (Nil) -> Nil end end.
 cons () -> fun (A) -> fun (List) -> fun (Cons) -> fun (Nil) -> ap(Cons,[A,ap(List,[Cons,Nil])]) end end end end.
 
-church (0) -> zero();
-church (I) -> fun (Succ) -> fun(Zero) -> ap(Succ,[ap(church(I-1),[Succ,Zero])]) end end.
-
-unchurch (N) -> ap(N,[inc(),0]).
-
-main()        -> io:format("Zero: ~p~n", [unchurch(zero())]),
-                 io:format("Test Big Numeral: ~p~n",   [unchurch(church(100000))]),
-                 io:format("Two: ~p~n",  [unchurch(ap(succ(),[ap(succ(),[zero()])]))]).
+nat   (0) -> zero();
+nat   (I) -> fun (Succ) -> fun(Zero) -> ap(Succ,[ap(nat(I-1),[Succ,Zero])]) end end.
+unnat (N) -> ap(N,[inc(),0]).
+main  ()  -> io:format("Zero: ~p~n", [unnat(zero())]),
+             io:format("Test Big Numeral: ~p~n",   [unnat(nat(100000))]),
+             io:format("Two: ~p~n",  [unnat(ap(succ(),[ap(succ(),[zero()])]))]).
