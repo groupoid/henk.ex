6c29e66919545539ddd16a715f65fbde40c41ec0
Namdak Tonpa <maxim@synrc.com>
2016-02-23 11:48:40 +0200

    pi

---
+++ src/pi.erl
@@ -0,0 +1,15 @@
+-module(pi).
+-description('Pi-calculus model based on Erlang axioms').
+-export([parse_transform/2]).
+-compile(export_all).
+
+parse_transform(Forms, _Options) ->
+    {_,_,_,Name} = hd(tl(Forms)),
+    io:format("Name: ~p~n",[Name]),
+    file:write_file(lists:concat([Name,".ast"]),list_to_binary(io_lib:format("~p",[Forms]))),
+    compile:forms(Forms,[binary,export_all]),
+    Forms.
+
+main() ->
+    io:format("Main: ~p~n",[0]).
+
