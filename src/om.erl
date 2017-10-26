e303f02318b6e1822bd5a1d3ac658493ba3008a9
Namdak Tonpa <maxim@synrc.com>
2017-10-26 19:13:53 +0300

    fix build

--- src/om.erl
+++ src/om.erl
@@ -108,9 +108,10 @@ pipe(L)      -> io:format("[~tp]~n",[L]), % workaround for trevis timeout break
 pass(0)      -> "PASSED";
 pass(X)      -> "FAILED " ++ integer_to_list(X).
 all(_)       -> all().
-all()        -> lists:flatten([ begin
+all()        -> X = lists:flatten([ begin
                 ets_clear(), ets_boot(), om:mode(M), om:scan() end || M <- allmodes() ]),
-                application:stop(om), application:start(om), om:mode("normal").
+                catch begin application:stop(om), application:start(om), om:mode("normal") end,
+                X.
 syscard(P)   -> [ {F} || F <- filelib:wildcard(name(mode(),P,"**/*")), filelib:is_dir(F) /= true ].
 wildcard(P)  -> Q = om:name(mode(),P), lists:flatten([ {A}
                      || {A,B} <- ets:tab2list(filesystem), lists:sublist(A,length(Q)) == Q ]).
