a3bfa1d066259f55f63bb4e43ea846be99e22e9c
Namdak Tonpa <maxim@synrc.com>
2017-10-31 14:06:57 +0200

    list case_on

--- src/om_extract.erl
+++ src/om_extract.erl
@@ -15,8 +15,10 @@ extr(X) -> om:restart(), om:mode(X), extract("priv/"++X).
 extract(X)  ->  O = replace(om:pname(X),"/","."),
                 save(X, [{attribute,1,module,om:atom(normal(O,X))},
                          {attribute,1,compile,export_all}] ++ [ begin
-%                         io:format("Trace: ~p~n",[{X,F}]),
-                   extract(F,om:norm(om:fst(om:erase(om:snd(om:parse(om:read(name(X,F))))))),1)
+                         Norm = om:norm(om:fst(om:erase(om:snd(om:parse(om:read(name(X,F))))))),
+                         Ext = extract(F,Norm,1),
+                         io:format("Trace: ~p~n",[{X,F,Ext}]),
+                         Ext
                    end || F <- element(2,file:list_dir(X)), not filelib:is_dir(name(X,F)) ] ++ [{eof,1}] ),
                 [  extract(name(X,Subdir)) || Subdir <- om:snd(file:list_dir(X)), filelib:is_dir(name(X,Subdir)) ],
                 ok.
